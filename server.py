0168faa5e4ac583e6c2543cae0c4841a8d93fff9
bahoo08 <bahoo08@list.ru>
2021-11-13 13:30:20 +0300

    Добавил обработку сразу нескольких клиентов

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
-import socket 
-
+import socket
+from _thread import start_new_thread
 
 class Server:
     def __init__(self) -> None:
@@ -9,27 +9,29 @@ class Server:
         self.tcp_ser_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.max_listen_val = 5
 
-
-    def run(self) -> None:
-        self.tcp_ser_sock.bind((self.ip, self.port))
-        self.tcp_ser_sock.listen(self.max_listen_val)
-        tcp_cli_sock, addr = self.tcp_ser_sock.accept()
-        print(f"{addr} has connected")
-
-        file_name = tcp_cli_sock.recv(self.buff_size).decode()
+    def sock_thread(self, socket):
+        file_name = socket.recv(self.buff_size).decode()
         print(file_name)
         f = open('server_files/' + file_name, 'wb')
-
-        # временный бесконечный цикл
         while True:
-            taked_data = tcp_cli_sock.recv(self.buff_size)
+            taked_data = socket.recv(self.buff_size)
 
             if not taked_data:
                 break
 
             f.write(taked_data)
+        socket.close()
+    
+    def run(self) -> None:
+        self.tcp_ser_sock.bind((self.ip, self.port))
+        self.tcp_ser_sock.listen(self.max_listen_val)
 
-        tcp_cli_sock.close()
+        while True:
+            tcp_cli_sock, addr = self.tcp_ser_sock.accept()
+            print(f"{addr} has connected")
+            start_new_thread(self.sock_thread, (tcp_cli_sock,))
+
+        
         self.tcp_ser_sock.close()
 
 
